cb8e7dda522ff3e8277b93273b494c04203d22ed
Mahesa Insan <68182275+MahesaInsan@users.noreply.github.com>
2023-12-16 22:33:57 +0700

    Update Connect.java
    add documentation

--- src/main/Connect.java
+++ src/main/Connect.java
@@ -1,24 +1,9 @@
 package main;
 
 import java.sql.*;
-/**
-* <strong>
-* --------------------------------------------------------------- <br>
-* | ISYS6197 - BUSINESS APPLICATION DEVELOPMENT | <br>
-* --------------------------------------------------------------- <br>
-* </strong>
-* <br>
-* Connect.java | This class is used for connection to MySQL database
-* <br>
-* Copyright 2019 - Bina Nusantara University
-* <br>
-* Software Laboratory Center | Laboratory Center Alam Sutera
-* <br>
-* Kevin Surya Wahyudi (SW16-2), All rights reserved.
-* <br>
-*/
+
 public final class Connect {
-	
+	//inisasi variable untuk connect sql
 	private final String USERNAME = "root"; // change with your MySQL username, the default username is 'root'
 	private final String PASSWORD = ""; // change with your MySQL password, the default password is empty
 	private final String DATABASE = "projectlab"; // change with the database name that you use
@@ -28,12 +13,8 @@ public final class Connect {
 	private Connection con;
 	private Statement st;
 	private static Connect connect;
-	
-	/**
-	* Constructor for Connect class
-	* <br>
-	* This class is used singleton design pattern, so this class only have one instance
-	*/
+
+	//constructor untuk melakukan koneksi
     private Connect() {
     	try {  
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -45,25 +26,13 @@ public final class Connect {
         	System.exit(0);
         }  
     }
-    
-	/**
-	* This method is used for get instance from Connect class
-	* @return Connect This returns instance from Connect class
-	*/
+
+	//singleton untuk connection
     public static synchronized Connect getConnection() {
-		/**
-		* If the connect is null then:
-		*   - Create the instance from Connect class
-		*   - Otherwise, just assign the previous instance of this class
-		*/
 		return connect = (connect == null) ? new Connect() : connect;
     }
 
-    /**
-	* This method is used for SELECT SQL statements.
-	* @param String This is the query statement
-	* @return ResultSet This returns result data from the database
-	*/
+	//function untuk melakukan execute query
     public ResultSet executeQuery(String query) {
         ResultSet rs = null;
     	try {
@@ -74,10 +43,7 @@ public final class Connect {
         return rs;
     }
 
-	/**
-	* This method is used for INSERT, UPDATE, or DELETE SQL statements.
-	* @param String This is the query statement
-	*/
+	//function untuk melakukan execute update
     public void executeUpdate(String query) {
     	try {
 			st.executeUpdate(query);
@@ -85,11 +51,8 @@ public final class Connect {
 			e.printStackTrace();
 		}
     }
-    
-	/**
-	* This method is used for SELECT, INSERT, UPDATE, or DELETE SQL statements using prepare statement.
-	* @param String This is the query statement
-	*/
+
+	//function untuk membuat prepare statement
     public PreparedStatement prepareStatement(String query) {
     	PreparedStatement ps = null;
     	try {
@@ -99,7 +62,5 @@ public final class Connect {
 		}
     	return ps;
     }
-    
-    
     
 }
