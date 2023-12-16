abf900d8286d3429fff2c2e83d321d5e5916b364
Mahesa Insan <68182275+MahesaInsan@users.noreply.github.com>
2023-12-16 17:06:56 +0700

    Update User.java

--- src/models/User.java
+++ src/models/User.java
@@ -14,6 +14,58 @@ public class User {
 	private int userAge;
 	private String userRole;
 	
+	public User(int userID, String userName, String userPassword, int userAge, String userRole) {
+		super();
+		this.userID = userID;
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.userAge = userAge;
+		this.userRole = userRole;
+	}
+	
+	public User() {
+	};
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
 	public void getAllUserData() {
 		System.out.println(this.userName + " " + this.userPassword + " " + this.userAge);
 	}
@@ -36,12 +88,31 @@ public class User {
 		ps.executeUpdate();
 	}
 	
-	public void changeUserRole(String userID, String newRole) {
-		
+	public static void changeUserRole(int userID, String newRole) throws SQLException{
+		Connect db = Connect.getConnection();
+		PreparedStatement ps = db.prepareStatement("UPDATE users SET role = ? WHERE id = ?");
+		ps.setString(1, newRole);
+		ps.setInt(2, userID);
+		ps.executeUpdate();
 	}
 	
 	public getAllTechnician() {
 		
+	}
+	
+	public static ArrayList<User> getAllUser() throws SQLException{
+		ArrayList<User> staffList = new ArrayList<User>();
+		Connect db = Connect.getConnection();
+		
+		PreparedStatement ps = db.prepareStatement("SELECT * FROM users");
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			staffList.add(new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+		}
+		
+		return staffList;
 	}
 	
 	public boolean checkUsername(String username) throws SQLException{
@@ -52,6 +123,19 @@ public class User {
 		if(rs.next()) {
 			return true;
 		}else return false;
+	}
+	
+	public static User checkStaffRole(String staffId) throws SQLException{
+		Connect db = Connect.getConnection();
+		int idNum = Integer.parseInt(staffId);
+		PreparedStatement ps = db.prepareStatement("SELECT * FROM `users` WHERE id = ?");
+		ps.setInt(1, idNum);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			return new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5));
+		}
+		return null;
 	}
 	
 	public User validateUser(String username, String password) throws SQLException {
