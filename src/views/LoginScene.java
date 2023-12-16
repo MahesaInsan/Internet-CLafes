68dca2f391cc4fca86f1480053910313f4e282ff
Mahesa Insan <68182275+MahesaInsan@users.noreply.github.com>
2023-12-16 22:12:03 +0700

    Update LoginScene.java
    add documentation and change password field

--- src/views/LoginScene.java
+++ src/views/LoginScene.java
@@ -8,11 +8,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LoginScene implements IErrorMessage{
+	//Melakukan inisialisasi instance
 	private static LoginScene instance;
 	
 	public static void setScene(Stage primaryStage) {
@@ -21,18 +23,20 @@ public class LoginScene implements IErrorMessage{
 		}
 		instance._setScene(primaryStage);
 	}
-	
+
+	//Inisasi variable javafx
 	Stage primaryStage;
 	Scene scene;
 	VBox container;
 	Label usernameLabel;
 	Label passwordLabel;
 	TextField usernameInput;
-	TextField passwordInput;
+	PasswordField passwordInput;
 	Button loginButton;
 	Hyperlink registerLink;
 	Label errorLabel;
-	
+
+	//Constructor login untuk memanggil function yang ada
 	private LoginScene() {
 		container = new VBox();
 		initializeUsername();
@@ -45,7 +49,8 @@ public class LoginScene implements IErrorMessage{
 		scene = new Scene(container);
 		scene.setRoot(container);
 	}
-	
+
+	//Inisialisasi untuk meminta input username
 	private void initializeUsername() {
 		HBox usernameDiv = new HBox();
 		usernameLabel = new Label("Username");
@@ -53,15 +58,17 @@ public class LoginScene implements IErrorMessage{
 		usernameDiv.getChildren().addAll(usernameLabel, usernameInput);
 		container.getChildren().add(usernameDiv);
 	}
-	
+
+	//Inisialisasi untuk meminta input password
 	private void initializePassword() {
 		HBox passwordDiv = new HBox();
 		passwordLabel = new Label("Password");
-		passwordInput = new TextField();
+		passwordInput = new PasswordField();
 		passwordDiv.getChildren().addAll(passwordLabel, passwordInput);
 		container.getChildren().add(passwordDiv);
 	}
-	
+
+	//Memberikan event handler untuk button login
 	private void initializeLogin() {
 		loginButton = new Button("Login");
 		loginButton.setOnAction(event ->{
@@ -78,7 +85,8 @@ public class LoginScene implements IErrorMessage{
 		});
 		container.getChildren().add(loginButton);
 	}
-	
+
+	//Memberikan hyperlink untuk memberikan opsi register
 	private void initializeRegisterLink() {
 		registerLink = new Hyperlink("Don't have an account? Register");
 		registerLink.setOnAction(event ->{
@@ -86,12 +94,14 @@ public class LoginScene implements IErrorMessage{
 		});
 		container.getChildren().add(registerLink);
 	}
-	
+
+	//Melakukan inisiasi scene dan mengassign primary stage
 	private void _setScene(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		primaryStage.setScene(scene);
 	}
 
+	//Memberikan display error
 	@Override
 	public void displayErrorMessage(String error) {
 		// TODO Auto-generated method stub
