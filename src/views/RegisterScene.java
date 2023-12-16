32d3d23615ff46a797a2492bffd411724c158c32
Mahesa Insan <68182275+MahesaInsan@users.noreply.github.com>
2023-12-16 22:16:55 +0700

    Update RegisterScene.java
    Add documentation + password field

--- src/views/RegisterScene.java
+++ src/views/RegisterScene.java
@@ -9,11 +9,13 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class RegisterScene implements IErrorMessage{
+	//Inisiasi instance
 	private static RegisterScene instance;
 	
 	public static void setScene(Stage primaryStage) {
@@ -22,7 +24,8 @@ public class RegisterScene implements IErrorMessage{
 		}
 		instance._setScene(primaryStage);
 	}
-	
+
+	//Inisiasi variable javafx
 	Stage primaryStage;
 	Scene scene;
 	VBox container;
@@ -31,13 +34,14 @@ public class RegisterScene implements IErrorMessage{
 	Label confPassLabel;
 	Label ageLabel;
 	TextField usernameInput;
-	TextField passwordInput;
-	TextField confPassInput;
+	PasswordField passwordInput;
+	PasswordField confPassInput;
 	Spinner<Integer> ageInput;
 	Button registerButton;
 	Hyperlink loginLink;
 	Label errorLabel;
-	
+
+	//Constructor untuk memanggil variable dan bagian dari register
 	private RegisterScene() {
 		container = new VBox();
 		initializeUsername();
@@ -52,7 +56,8 @@ public class RegisterScene implements IErrorMessage{
 		scene = new Scene(container);
 		scene.setRoot(container);
 	}
-	
+
+	//Menampilkan div untuk input username
 	private void initializeUsername() {
 		HBox usernameDiv = new HBox();
 		usernameLabel = new Label("Username");
@@ -60,23 +65,26 @@ public class RegisterScene implements IErrorMessage{
 		usernameDiv.getChildren().addAll(usernameLabel, usernameInput);
 		container.getChildren().add(usernameDiv);
 	}
-	
+
+	//Menampilkan div untuk input password
 	private void initializePassword() {
 		HBox passwordDiv = new HBox();
 		passwordLabel = new Label("Password");
-		passwordInput = new TextField();
+		passwordInput = new PasswordField();
 		passwordDiv.getChildren().addAll(passwordLabel, passwordInput);
 		container.getChildren().add(passwordDiv);
 	}
-	
+
+	//Menampilkan div untuk input confirm password
 	private void initializeConfirmPassword() {
 		HBox confPassDiv = new HBox();
 		confPassLabel = new Label("Confirm Password");
-		confPassInput = new TextField();
+		confPassInput = new PasswordField();
 		confPassDiv.getChildren().addAll(confPassLabel, confPassInput);
 		container.getChildren().add(confPassDiv);
 	}
-	
+
+	//Menampilkan div untuk input umur
 	private void initializeAge() {
 		HBox ageDiv = new HBox();
 		ageLabel = new Label("Age");
@@ -84,7 +92,8 @@ public class RegisterScene implements IErrorMessage{
 		ageDiv.getChildren().addAll(ageLabel, ageInput);
 		container.getChildren().add(ageDiv);
 	}
-	
+
+	//Memberikan event handler untuk register button
 	private void initializeRegister() {
 		registerButton = new Button("Register");
 		registerButton.setOnAction(event ->{
@@ -103,7 +112,8 @@ public class RegisterScene implements IErrorMessage{
 		});
 		container.getChildren().add(registerButton);
 	}
-	
+
+	//Memberikan hyperlink untuk opsi login bagi user
 	private void initializeLogin() {
 		loginLink = new Hyperlink("Already have an account? login");
 		loginLink.setOnAction(event ->{
@@ -111,12 +121,14 @@ public class RegisterScene implements IErrorMessage{
 		});
 		container.getChildren().add(loginLink);
 	}
-	
+
+	//Melakukan inisasi scene dan mengassign primary stage
 	private void _setScene(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		primaryStage.setScene(scene);
 	}
 
+	//Memberikan display berupa error message
 	@Override
 	public void displayErrorMessage(String error) {
 		// TODO Auto-generated method stub
