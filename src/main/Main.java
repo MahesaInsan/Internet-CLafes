9ffb6b0d57c2f3caa050ee13d99b1d96d0bda9f5
Mahesa Insan <68182275+MahesaInsan@users.noreply.github.com>
2023-12-16 22:36:52 +0700

    Update Main.java
    add documentation

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,24 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import views.DisplayAllPCScene;
 import views.RegisterScene;
-import views.TechnicianJobView;
-import views.ViewAllJobScene;
-import views.ViewAllStaffScene;
-import views.ViewAllTransactionScene;
+
+//Group 7
+//2501981024	MAHESA INSAN RAUSHANFIKIR
+//2502012312	NAUFAL DAFFA RYQUELME
+//2501984096	KENNETH MATTHEW
+//2501986422	KENRICK PANCA DEWANTO
 
 public class Main extends Application{
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	//Starting application with register scene
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		TechnicianJobView.setScene(primaryStage);
+		RegisterScene.setScene(primaryStage);
 		primaryStage.show();
 	}
 }
